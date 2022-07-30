b318e79cb6fdb6137e962da334109a6115cab49a
gary.fu <xuebaiyouling@gmail.com>
2022-07-30 16:14:06 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # utools-file-to-folder
 uTools工具的插件，使用超级面板，选中文件或文件夹并自动移动或复制到一个新建文件夹中，也可以指定已经存在的文件夹
+
+### 截图
+
+使用超级面板，选中文件
+
+![image-20220730153516198](https://git.mengqingpo.com:8888/fugary/blogpic/uploads/79c075dd4a10efe4d91276728427780c/image-20220730153516198.png)
+
+输入文件名等操作
+
+![image-20220730154722043](https://git.mengqingpo.com:8888/fugary/blogpic/uploads/925a9dafa9adf550240f79484bb7a4d2/image-20220730154722043.png)
+
+然后执行移动或者复制。
---
+++ assets/index.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+    Vue.createApp({
+        data() {
+            return {
+                filesData: [],
+                folderName: ''
+            }
+        },
+        methods: {
+            selectFolderName() {
+                const directoryNames = utools.showOpenDialog({ properties: ['openDirectory'] });
+                if (directoryNames && directoryNames.length) {
+                    this.folderName = directoryNames[0];
+                }
+            },
+            clearFileNames() {
+                this.filesData = [];
+                this.folderName = '';
+            },
+            moveOrCopyFile(copy) {
+                window.moveOrCopyFile(this.filesData, this.folderName, copy);
+                utools.showNotification(copy ? '文件或文件夹复制完成' : '文件或文件夹移动完成');
+            },
+            dragFiles(e) {
+                if (e.dataTransfer && e.dataTransfer.files) {
+                    for (let i = 0; i < e.dataTransfer.files.length; i++) {
+                        const dragFile = e.dataTransfer.files[i];
+                        const file = {
+                            name: dragFile.name,
+                            path: dragFile.path
+                        };
+                        window.checkDragFile(file);
+                        if(!this.checkExistsFile(file)){
+                            this.filesData.push(file);
+                        }
+                    }
+                }
+            },
+            checkExistsFile(file) {
+                return !!this.filesData.find(existsFile => existsFile.path === file.path);
+            },
+            removeFileFromList(file) {
+                const idx = this.filesData.indexOf(file);
+                this.filesData.splice(idx, 1);
+            }
+        },
+        mounted() {
+            utools.onPluginEnter(({ code, type, payload, optional }) => {
+                console.log('用户进入插件', code, type, payload)
+                if (type === "files") {
+                    this.filesData = payload || []
+                    this.moveFiles(payload);
+                }
+            });
+        }
+    }).mount("#app");
+}
